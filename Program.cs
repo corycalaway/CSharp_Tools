76cd37f090909dbbb72006ca406d92dc4d8c9561
Cory Calaway <cory.c.calaway@gmail.com>
2021-06-01 09:20:11 -0600

    started c#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// creating the app 
+//dotnet new console 
+// run with ctrl+F5
+
+
+namespace C__tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yay!");
+            Console.Beep();
+      
+
+
+        }
+    }
+}
